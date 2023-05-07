522d6dad0616b784cc34f880e9fcaf7d0b7d6ec1
it3037 <rakeshpal51196@gmail.com>
2023-05-07 11:07:56 +0530

    sensor updated

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,20 +1,34 @@
-import pymongo
-from sensor.constant.database import DATABASE_NAME
-#from sensor.constant.env_variable import MONGODB_URL_KEY
-import certifi
 import os
+import sys
+
+import certifi
+import pymongo
+
+from sensor.constant.database import DATABASE_NAME
+from sensor.constant.env_variable import MONGODB_URL_KEY
+from sensor.exception import SensorException
+
 ca = certifi.where()
+
 
 class MongoDBClient:
     client = None
+
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
-
             if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://rakesh_18:mongo7715@cluster0.jrvckka.mongodb.net/?retryWrites=true&w=majority" #os.getenv(MONGODB_URL_KEY)
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+
+                if mongo_db_url is None:
+                    raise Exception(f"Environment key: {MONGODB_URL_KEY} is not set.")
+
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+
             self.client = MongoDBClient.client
+
             self.database = self.client[database_name]
+
             self.database_name = database_name
+
         except Exception as e:
-            raise e
+            raise SensorException(e, sys)
